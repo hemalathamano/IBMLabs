c995aff377b8a251d61b2e1d72ab2930f23cd04c
hemalathamano <54215560+hemalathamano@users.noreply.github.com>
2020-02-10 22:27:21 -0800

    Add files via upload

---
+++ Calci.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter a number1 : "))
+number2 = int(input("Enter a number1 : "))
+ADD = number1+number2
+SUB = number1-number2
+MUL = number1*number2
+DIV = float(number1)/float(number2)
+print("ADD : ",ADD)
+print("SUB : ",SUB)
+print("MUL : ",MUL)
+print("DIV : ",DIV)
+
