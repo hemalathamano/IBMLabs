ea04b527c757d59671aa6f3247fda80dfd9e8648
hemalathamano <54215560+hemalathamano@users.noreply.github.com>
2020-02-26 19:45:12 +0530

    Add files via upload

---
+++ literalcollection.py
@@ -0,0 +1,10 @@
+fruits = ["watermelon","apple","orange"]
+vowels = {'a','e','i','o'}
+numbers = (10,20,30,40,50)
+alphabets = {'a':'ant','b':'ball','c':"cat"}
+
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
+
